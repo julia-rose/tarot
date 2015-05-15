470e73e6214f8b185b2a66b0e13689678e16c428
Julia Rose <julmrose@gmail.com>
2015-05-15 01:56:32 -0400

    add css and front-end stylin'

--- tarot_app/app/controllers/readings_controller.rb
+++ tarot_app/app/controllers/readings_controller.rb
@@ -5,7 +5,29 @@ class ReadingsController < ApplicationController
 	end
 
 	def show
-		@reading = current_user.reading.find(params[:id])
+		@reading = current_user.readings.find(params[:id])
+
+		@placements = @reading.placements.all
+	end
+
+	def new
+		@reading = current_user.readings.new
+	end
+
+	def create
+		@reading = current_user.readings.create(reading_params)
+		@deck = Card.all.shuffle.take(10)
+
+		@deck.each_with_index do |card, index|
+			@reading.placements.create(card_id: card.id, position: (index + 1))
+		end
+
+		redirect_to user_reading_path(@reading)
+	end
+
+	private
+	def reading_params
+		params.require(:reading).permit(:topic, :notes)
 	end
 
 end
